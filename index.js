1e8831ec5c660fb37b9e9c9c7467634491fdb99a
harris <sirrah1337@gmail.com>
2022-03-29 11:40:53 +0200

    update

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ for (var i = 0; i < numberOfDrums; i++) {
         var buttonInnerHTML = this.innerHTML
         makeSound(buttonInnerHTML)
 
+        buttonAnimation(buttonInnerHTML)
     })
 }
 
 // Detecting Keyboard Press
 document.addEventListener("keydown", function(event) {
     makeSound(event.key)
+    buttonAnimation(event.key)
 })
 
 
@@ -59,4 +61,14 @@ function makeSound(key) {
         default:
             break;
     }
+}
+
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey)
+    activeButton.classList.add("pressed")
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed")
+    }, 100);
 }
